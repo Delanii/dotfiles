d2318daac91a738c6684775f26bc1710da28a89f
Delanii <46751732+Delanii@users.noreply.github.com>
2020-10-22 14:59:06 +0200

    Update init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -64,12 +64,12 @@
        ;; objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
-       snippets
-                                        ; my elves. They type so I don't have to
+       snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired
+        +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
